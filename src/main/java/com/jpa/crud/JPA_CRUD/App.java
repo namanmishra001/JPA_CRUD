8051e1197e7c89470fe7366b6b08a23e69934d6f
Naman <Naman@192.168.0.18>
2018-01-13 20:37:37 +0530

    Initial Commit to fresh project

---
+++ src/main/java/com/jpa/crud/JPA_CRUD/App.java
@@ -0,0 +1,13 @@
+package com.jpa.crud.JPA_CRUD;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
